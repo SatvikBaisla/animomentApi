704bd6d7e81aa523d7e85b32a5957991ff83ac8a
Satvik Baisla <satvikbaisla@Satviks-MacBook-Air.local>
2025-06-05 02:57:06 +0530

    Initial Commit for api

--- obj/Debug/net8.0/animomentapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/animomentapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("animomentapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62a0142ad569657b605e73b49f6ed6e5bca8938")]
 [assembly: System.Reflection.AssemblyProductAttribute("animomentapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("animomentapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
